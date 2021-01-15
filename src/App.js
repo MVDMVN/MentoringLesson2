bf4a4307ebe903cfd02412818e682409f8b12a0d
Andrey Melnichuk <edifens@gmail.com>
2021-01-15 20:30:30 +0200

    second task finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import React from 'react';
 import './App.scss';
 import food from "./food.json";
 
 import CardItem from "./components/CardItem";
 
 function App() {
+  const [searchValue, setSearchValue] = React.useState('');
+
   return (
     <div className="app">
       <div className="app-container">
-        {food.map((item) => (
-          <CardItem title={item.title} description={item.description} image={item.image} />
-        ))}
+      <div className="input-container">
+        <input type="text" className="form-control" placeholder="Поиск" value={searchValue} onChange={(e) => setSearchValue(e.target.value)}/>
+      </div>
+        <div className="cards-container">
+          {food
+            .filter(
+              (data) =>
+                data.title.toLowerCase().includes(searchValue.toLowerCase()) ||
+                data.description.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((item, index) => (
+            <CardItem key={index} title={item.title} description={item.description} image={item.image} />
+          ))}
+        </div>
       </div>
     </div>
   );
