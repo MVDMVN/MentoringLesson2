02ff37cb9121c5d7593ca2df193a7c00fe797af1
Andrey Melnichuk <edifens@gmail.com>
2021-01-16 00:24:09 +0200

    changes after code review

--- src/App.js
+++ src/App.js
@@ -1,62 +1,71 @@
-import React from 'react';
-import axios from 'axios';
-import './App.scss';
+import React from "react";
+import axios from "axios";
+import "./App.scss";
 
-import UserCard from './components/UserCard'
+import UserCard from "./components/UserCard";
 
 function App() {
-  function getApiUrl(apiPage) {
-    return 'https://5c3755177820ff0014d92711.mockapi.io/users?page=' + apiPage + '&limit=10';
-  }
-
-  const [searchValue, setSearchValue] = React.useState('');
+  const [searchValue, setSearchValue] = React.useState("");
   const [userInfo, setUserInfo] = React.useState([]);
-  const [running, setRunning] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
   const [apiPage, setApiPage] = React.useState(1);
   const [isAllUsersDownloaded, setAllUsersDownloaded] = React.useState(false);
 
-  React.useEffect(() => {
-    axios.get(getApiUrl(apiPage)).then((response) => {
-      setUserInfo(response.data);
-    });
-  }, []);
-
-  React.useEffect(() => {
-    setApiPage(apiPage + 1);
-    axios.get(getApiUrl(apiPage)).then((response) => {
-      if(response.data.length < 1) {
-        setAllUsersDownloaded(true);
-      }
-      let newUserInfo = userInfo.concat(response.data);
-      setUserInfo(newUserInfo);
-    })
-    .finally(() => {
-      setRunning(false);
-    })
-  }, [running]);
-
-  const toggleRunning = () => {
-    setRunning(true);
+  function getApiUrl(apiPage) {
+    return (
+      "https://5c3755177820ff0014d92711.mockapi.io/users?page=" +
+      apiPage +
+      "&limit=10"
+    );
   }
+
+  function addApiPageNumber() {
+    setApiPage(apiPage + 1);
+  }
+
+  React.useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(getApiUrl(apiPage))
+      .then((response) => {
+        if (!response.data.length) {
+          setAllUsersDownloaded(true);
+        } else {
+          setUserInfo(userInfo.concat(response.data));
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [apiPage]);
+
   return (
     <div className="app">
       <div className="app-container">
-        <input type="text" placeholder="Поиск пользователя..." value={searchValue} onChange={(e) => {setSearchValue(e.target.value)}}/>
-          <ul className="users">
-            { userInfo
-              .filter((info) => info.name.toLowerCase().includes(searchValue.toLowerCase()) ||
-              info.email.toLowerCase().includes(searchValue.toLowerCase())
-              )
-              .map((user, index) =>
-                <UserCard key={index} name={user.name} email={user.email} />
-              )}
-          </ul>
-        {
-          !isAllUsersDownloaded ? (
-            <button onClick={toggleRunning}>{running ? 'Waiting...' : 'Add' +
-              ' User'}</button>
-          ) : ('')
-        }
+        <input
+          type="text"
+          placeholder="Поиск пользователя..."
+          value={searchValue}
+          onChange={(e) => {
+            setSearchValue(e.target.value);
+          }}
+        />
+        <ul className="users">
+          {userInfo
+            .filter(
+              (info) =>
+                info.name.toLowerCase().includes(searchValue.toLowerCase()) ||
+                info.email.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((user) => (
+              <UserCard key={user.id} name={user.name} email={user.email} />
+            ))}
+        </ul>
+        {!isAllUsersDownloaded && (
+          <button onClick={addApiPageNumber}>
+            {isLoading ? "Waiting..." : "Next 10 Users"}
+          </button>
+        )}
       </div>
     </div>
   );
